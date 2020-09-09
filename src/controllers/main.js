ec9924e329fd9354b435073066f79b61ae719d26
Andojo Mack <andojomack@gmail.com>
2020-09-09 20:43:04 -0300

    synced permissions files

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,18 +1,32 @@
 token = localStorage.getItem("token")
-gebruiker_type_id = JSON.parse(localStorage.getItem("nhs_user")).gebruiker_type_id
+gebruiker_type = localStorage.getItem("gebruiker_type");
+
+function redirectIfNoToken() {
+    if (token == null) {
+        window.location.replace("../../../index.html");
+        alert("U Moet Eerst Inloggen!");
+    }
+}
+
+
 
 function logout() {
     localStorage.removeItem("token");
     localStorage.removeItem("gebruiker_id");
-    // localStorage.removeItem("gebruiker_type_id");
+    localStorage.removeItem("gebruiker_type");
     localStorage.removeItem("nhs_user");
     window.location.replace("../../../index.html");
 }
 
 
+
+
 function getName() {
     return `${JSON.parse(localStorage.getItem("nhs_user")).full_name}`
 }
+
+
+redirectIfNoToken()
 
 function refreshSelect(el) {
     let instance = M.FormSelect.getInstance(el)
--- src/controllers/permissions.js
+++ src/controllers/permissions.js
@@ -1,10 +1,10 @@
-const gebruiker_id = JSON.parse(localStorage.getItem("nhs_user")).id;
-const current_token = localStorage.getItem("token");
-
-const myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer " + current_token);
-
 function redirectIfNotPermitted() {
+  const gebruiker_id = localStorage.getItem("gebruiker_id");
+  const current_token = localStorage.getItem("token");
+
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + current_token);
+
   fetch(`http://127.0.0.1:3000/gebruiker/${gebruiker_id}`, {
     method: "GET",
     headers: myHeaders,
@@ -15,33 +15,20 @@ function redirectIfNotPermitted() {
     .then((res) => res.json())
     .then((data) => {
       // console.log(data.data.type);
-      type_id = JSON.parse(localStorage.getItem("nhs_user")).gebruiker_type_id;
-      let type;
-      if (type_id == 1) {
-        type = "admin";
-      } else if (type_id == 2) {
-        type = "docent";
-      } else if (type_id == 3) {
-        type = "student";
-      }
+
+      baseURL = window.location.pathname.split('/').filter(item => item)
+      type = baseURL[baseURL.length - 2].toLowerCase()
 
       if (type != data.data.type) {
-        console.log(data.data.type);
-        localStorage.removeItem("nhs_user");
-        localStorage.setItem("nhs_user", JSON.stringify(data.data));
-        window.location.replace("../../../index.html");
-        alert("U Mag deze pagina niet bezoeken!");
+        console.log(data.data.type)
+        localStorage.removeItem("gebruiker_type")
+        localStorage.setItem("gebruiker_type", data.data.type)
+        window.location.replace("../../../index.html")
+        alert("U Mag deze pagina niet bezoeken!")
       }
     })
     .catch((err) => {
       console.error(err);
     });
   return false;
-}
-
-function redirectIfNoToken() {
-  if (current_token == null) {
-    window.location.replace("../../../index.html");
-    alert("U Moet Eerst Inloggen!");
-  }
-}
+} 
