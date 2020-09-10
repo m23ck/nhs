bfb5b4fdad5bc054f39659b85f412ad612cd695a
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2020-09-10 07:55:10 -0300

    added chart.js charts to dashboards

---
+++ src/js/charts/studentDashboard.js
@@ -0,0 +1,51 @@
+const current_token2 = localStorage.getItem("token");
+
+const myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer " + current_token2);
+
+function getData() {
+    fetch('http://127.0.0.1:3000/', {
+        method: "GET",
+        headers: myHeaders,
+        mode: "cors",
+        cache: "default",
+    }).then
+}
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
