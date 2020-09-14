e504a7f9da46df27ed1f11a3f1c38c0038e4cc83
Andojo Mack <andojomack@gmail.com>
2020-09-13 22:52:46 -0300

    view submissions by class / roadmap and status

--- src/controllers/assignmentSubmissionsController.js
+++ src/controllers/assignmentSubmissionsController.js
@@ -70,12 +70,61 @@ function getRoadmaps() {
   // return false;
 }
 
+
+function changeSubmission(assignment_submission_id, status){
+  let VALUE = JSON.stringify({status: status, assignment_submission_id: assignment_submission_id})
+  console.log(VALUE)
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + current_token2);
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Accept", "application/json");
+
+  fetch(`http://127.0.0.1:3000/assignment_submission/${assignment_submission_id}`, {
+    method: "PUT",
+    headers: myHeaders,
+    mode: "cors",
+    cache: "default",
+    body: VALUE,
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log("Success", res);
+      location.reload();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  return false;
+}
+
+
+function approveSubmission(td){
+  selectedRow = td.parentElement.parentElement;
+  let assignment_submission_id = selectedRow.cells[0].innerHTML;
+
+  let status = "approved"
+  changeSubmission(assignment_submission_id, status)
+
+}
+
+function rejectSubmission(td){
+  selectedRow = td.parentElement.parentElement;
+  let assignment_submission_id = selectedRow.cells[0].innerHTML;
+  
+  let status = "rejected"
+  changeSubmission(assignment_submission_id, status)
+}
+
+
+
 function fillSelects() {
   getKlassen();
   getRoadmaps();
 }
 
 function requestSubmissions() {
+  
   let form = document.forms["requestSubmissionsForm"];
   let fd = new FormData(form);
   let data = {};
@@ -108,10 +157,11 @@ function requestSubmissions() {
         data.data.forEach((i) => {
           body += "<tr>";
           body += "<td>" + i.id + "</td>";
-        //   body += "<td>" + i.vak_naam + "</td>";
+          body += "<td>" + i.naam +" "+ i.voornaam + "</td>";
+          body += "<td>" + i.assignment_naam + "</td>";
           body += `<td>
-                      <a class='modal-trigger' href='#modal_update_vak' title='Wijzigen' data-toggle='tooltip' style='cursor: pointer;' onclick='return getData(this)'><i class='small material-icons' style='color: #ffd600;'>edit</i></a>
-                      <a title='Verwijderen' data-toggle='tooltip' style='cursor: pointer;' onclick='return deleteCheck(this)'><i class='small material-icons' style='color: #c62828;'>delete</i></a>
+                      <a title='Approve' data-toggle='tooltip' style='cursor: pointer;' onclick='return approveSubmission(this)'><i class='small material-icons' style='color: blue;'>mood</i></a>
+                      <a title='Reject' data-toggle='tooltip' style='cursor: pointer;' onclick='return rejectSubmission(this)'><i class='small material-icons' style='color: #c62828;'>mood_bad</i></a>
                   </td>`;
           body += "</tr>";
         });
