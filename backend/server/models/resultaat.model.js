d8ef98a7166ec5c9abdf6d81f0e06f85eba60713
Andojo Mack <andojomack@gmail.com>
2020-09-15 23:43:44 -0300

    getSpecificSubmissions leaves out approved ones now / getResultaten

--- backend/server/models/resultaat.model.js
+++ backend/server/models/resultaat.model.js
@@ -1,26 +1,27 @@
 const pool = require("../../config/config");
 
 module.exports = {
-  createResultaat: (data, callBack) => {
+  getResultaten: (callBack) => {
     pool.query(
-      'insert into resultaat(assignment_id, student_id, type, status) values(?,?,?,?)',
-      [
-          data.assignment_id,
-          data.student_id,
-          data.type,
-          data.status,
-        ],
-      (error, results, fields) => {
-        if (error) {
-          return callBack(error);
-        }
-        return callBack(null, results);
-      }
-    );
-  },
-  getResultaten: callBack => {
-    pool.query(
-      `select * from resultaat`,
+      `SELECT
+      gebruiker.naam,
+      gebruiker.voornaam,
+      assignment.assignment_naam,
+      assignment.omschrijving,
+      assignment.punten,
+      roadmap.roadmap_naam,
+      vak.vak_naam,
+      klas.klas_naam
+  FROM
+      resultaat
+  LEFT JOIN assignment_submission ON assignment_submission_id = assignment_submission.id
+  LEFT JOIN gebruiker ON assignment_submission.student_id = gebruiker.id
+  LEFT JOIN assignment ON assignment_submission.assignment_id = assignment.id
+  LEFT JOIN roadmap ON assignment.roadmap_id = roadmap.id
+  LEFT JOIN vak ON assignment.vak_id = vak.id
+  LEFT JOIN klas_roadmaps ON roadmap.id = klas_roadmaps.roadmap_id
+  LEFT JOIN jaar_klas ON klas_roadmaps.jaar_klas_id = jaar_klas.id
+  LEFT JOIN klas ON jaar_klas.klas_id = klas.id`,
       [],
       (error, results, fields) => {
         if (error) {
@@ -32,7 +33,25 @@ module.exports = {
   },
   getResultaatById: (id, callBack) => {
     pool.query(
-      `select * from resultaat where id = ?`,
+      `SELECT
+      gebruiker.naam,
+      gebruiker.voornaam,
+      assignment.assignment_naam,
+      assignment.omschrijving,
+      assignment.punten,
+      roadmap.roadmap_naam,
+      vak.vak_naam,
+      klas.klas_naam
+  FROM
+      resultaat
+  LEFT JOIN assignment_submission ON assignment_submission_id = assignment_submission.id
+  LEFT JOIN gebruiker ON assignment_submission.student_id = gebruiker.id
+  LEFT JOIN assignment ON assignment_submission.assignment_id = assignment.id
+  LEFT JOIN roadmap ON assignment.roadmap_id = roadmap.id
+  LEFT JOIN vak ON assignment.vak_id = vak.id
+  LEFT JOIN klas_roadmaps ON roadmap.id = klas_roadmaps.roadmap_id
+  LEFT JOIN jaar_klas ON klas_roadmaps.jaar_klas_id = jaar_klas.id
+  LEFT JOIN klas ON jaar_klas.klas_id = klas.id where id = ?`,
       [id],
       (error, results, fields) => {
         if (error) {
@@ -42,47 +61,7 @@ module.exports = {
       }
     );
   },
-  getResultaatByAssignmentId: (assignment_id, callBack) => {
-    pool.query(
-      `select * from resultaat where assignment_id = ?`,
-      [assignment_id],
-      (error, results, fields) => {
-        if (error) {
-          return callBack(error);
-        }
-        return callBack(null, results[0]);
-      }
-    );
-  },
-  getResultaatByStudentId: (student_id, callBack) => {
-    pool.query(
-      `select * from resultaat where student_id = ?`,
-      [student_id],
-      (error, results, fields) => {
-        if (error) {
-          return callBack(error);
-        }
-        return callBack(null, results[0]);
-      }
-    );
-  },
-  updateResultaat: (data, id, callBack) => {
-    pool.query(
-      'update resultaat set naam = ?, jaar = ?, resultaatsendocent_id = ? where id = ?',
-      [
-        data.naam,
-        data.jaar,
-        data.resultaatsendocent_id,
-        id
-      ],
-      (error, results, fields) => {
-        if (error) {
-          return callBack(error);
-        }
-        return callBack(null, results[0]);
-      }
-    );
-  },
+
   deleteResultaat: (id, callBack) => {
     pool.query(
       `delete from resultaat where id = ?`,
@@ -94,5 +73,5 @@ module.exports = {
         return callBack(null, results[0]);
       }
     );
-  }
+  },
 };
