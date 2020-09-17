1d2a713f11ae02513a59850a16a882c18a626f19
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2020-09-17 00:54:57 -0300

    added form change detection

--- src/controllers/gegevensController.js
+++ src/controllers/gegevensController.js
@@ -1,13 +1,28 @@
 
 const getUserInfo =
     ({ naam, voornaam, email, telefoon, adres }) => {
-        return [naam, voornaam, email, telefoon, adres]
+        return { naam, voornaam, email, telefoon, adres }
     }
 
 const user_info = getUserInfo(JSON.parse(localStorage.nhs_user))
-const inputs = document.getElementById("gegevensForm").querySelectorAll("input")
+const form = document.getElementById("gegevensForm")
+const inputs = form.querySelectorAll("input")
 
-for (let [index, input] of inputs.entries()) {
-    input.value = user_info[index]
+for (let input of inputs) {
+    input.value = user_info[input.id]
+    input.addEventListener("keyup", checkFormChange)
+}
+
+
+
+function checkFormChange() {
+    for (let input of inputs) {
+        if (user_info[input.id] != input.value) {
+            document.getElementById("submit").removeAttribute("disabled")
+            break
+        } else {
+            document.getElementById("submit").setAttribute("disabled", true)
+        }
+    }
 }
 
